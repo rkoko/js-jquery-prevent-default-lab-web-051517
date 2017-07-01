35cb1b326681d7c6bff80166df62aee0305f75e7
rkoko <rinda.ko@gmail.com>
2017-07-01 17:03:28 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  submitForm()
 });
 
 // define functions here
+function submitForm(){
+  $('form').on('submit', function(event){
+  var item = $('#item').val();
+  debugger
+  $('#list ol').append('<li>'+item+'</li>')
+  event.preventDefault();
+}
+)}
